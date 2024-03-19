bf56b11727b06f1067ddba6788885fffc67d1ebc
Husain Hakim <142593070+husainhakim@users.noreply.github.com>
2024-03-19 17:47:12 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
 public class fibonacci {
     public static void main(String[] args) {
-        int n = 10;
-        int a = 0, b = 1;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=scanner.nextInt();
+        int first = 0, second = 1;
         System.out.print("Fibonacci Series: ");
         for (int i = 0; i < n; i++) {
-            System.out.print(a + " ");
-            int sum = a + b;
-            a = b;
-            b = sum;
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
         }
     }
 }
+
