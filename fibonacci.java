085cedf72062d72207cad3a81fd1ae40130700d7
Husain Hakim <142593070+husainhakim@users.noreply.github.com>
2024-03-19 17:40:56 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,13 @@
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 10;
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+}
